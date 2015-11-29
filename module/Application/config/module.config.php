6bc30786ff133cc4052499fad58e9859daf386a7
Ricardo Jesus Ruiz Cruz <rrcfesc@gmail.com>
2015-11-28 22:51:05 -0600

    Revert "Cambio a twig, asi como integracion de primeras pruebas, asi como empezando dashboard"
    This reverts commit d867ec20104f55e8ab7f34939f93c34a20cdb335.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -9,9 +9,9 @@ return array(
         ),
     ),
     'controller_plugins' => array(
-        /*'invokables' => array(
+        'invokables' => array(
             'Params' => 'Application\Controller\Plugin\Params',
-        ),*/
+        ),
     ),
     'router' => array(
         'routes' => array(
@@ -55,22 +55,18 @@ return array(
                     ),
                 ),
             ),
-            'api' => array(
-                'type' => 'segment',
+            'findme' => array(
+                'type'    => 'Literal',
                 'options' => array(
-                    'route' => '/api/[:action]',
-                    /*'constraints' => array(
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id' => '[a-zA-Z0-9_-]*',
-                        'xmln' => '[a-zA-Z0-9_-]*',
-                        'xmlc' => '[*a-zA-Z0-9_-]*',
-                    ),*/
+                    'route'    => '/apiDevice',
+                    'constraints' => array(
+                        'id'     => '[0-9]+',
+                    ),
                     'defaults' => array(
                         'controller' => 'Application\Controller\Api',
-                        'action'     => 'index',
-                    )
+                    ),
                 ),
-            )
+            ),
         ),
     ),
     'service_manager' => array(
@@ -102,8 +98,8 @@ return array(
             'not_found_template'       => 'error/404',
             'exception_template'       => 'error/index',
             'template_map' => array(
-                'layout/layout'           => __DIR__ . '/../view/layout/layout.twig',
-                'application/index/index' => __DIR__ . '/../view/application/index/index.twig',
+                'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+                'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
                 'error/404'               => __DIR__ . '/../view/error/404.phtml',
                 'error/index'             => __DIR__ . '/../view/error/index.phtml',
             ),
